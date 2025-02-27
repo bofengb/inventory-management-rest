50d3f3187094bbe172f983b06cbcd1ad12f14237
coco-kit <bofg.fu@outlook.com>
2025-02-27 20:13:23 -0330

    Add permission

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -162,6 +162,7 @@ data "aws_iam_policy_document" "rds" {
       "rds:CreateDBInstance",
       "rds:DeleteDBInstance",
       "rds:ListTagsForResource",
+      "rds:AddTagsToResource",
       "rds:ModifyDBInstance"
     ]
     resources = ["*"]
